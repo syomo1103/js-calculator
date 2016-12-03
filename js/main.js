43f8c46133482c4df845c80ebc8394149d16ccae
Stephanie Yomorta <stephanie.yomorta@gmail.com>
2016-12-02 18:44:43 -0800

    calculator works

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ var displayEl = document.getElementById('display');
 
 /*---event listeners ---*/
 
-
-
-
+document.getElementById('calc')
+  .addEventListener('click', handleClick);
 
 /*---functions ---*/
 
@@ -18,8 +17,73 @@ function initalize() {
 }
 
 function updateDisplay() {
-  var text = result || input || '0.';
+  var text = result || input || '0';
+  text += input.includes('.') ? '' : '.';
   displayEl.textContent = text;
+  if (result) {
+    input = '';
+    op = firstNum = result = null;
+  }
+}
+
+function handleClick(evt) {
+  var clickedEl = evt.target;
+  if (clickedEl.id === 'display') return;
+    switch (clickedEl.textContent) {
+      case 'CA':
+        initalize();
+        break;
+      case '←':
+        input = input.substr(0, input.length - 1);
+        break;
+      case '÷':
+        assignOp(div);
+        break;
+      case '×':
+        assignOp(mult);
+        break;
+      case '+':
+        assignOp(add);
+        break;
+      case '-':
+        assignOp(sub);
+        break;
+      case '=':
+        if (!op || !input) return;
+        result = op(parseFloat(firstNum), parseFloat(input));
+        break;
+      case '.':
+        input += input.includes('.') ? '' : '.';
+        break;
+      default:
+        input += clickedEl.textContent;
+  }
+  updateDisplay();
+}
+
+function assignOp(clickedOp) {
+  if (!input) return;
+  op = clickedOp;
+  firstNum = input;
+  input = '';
 }
 
 initalize();
+
+/*---operator functions ---*/
+
+function add(x, y) {
+  return x + y;
+}
+
+function sub(x, y) {
+  return x - y;
+}
+
+function mult(x, y) {
+  return x * y;
+}
+
+function div(x, y) {
+  return x / y;
+}
